fdaf514378b84816cd80d6cab9d927af8f10123b
Robert James <robjames970@gmail.com>
2022-03-12 17:51:16 +0000

    refactor

--- src/roman-numerals/roman-numerals.ts
+++ src/roman-numerals/roman-numerals.ts
@@ -1,68 +1,34 @@
-enum numeralsEnum {
-  one = "I",
-  four = "IV",
-  five = "V",
-  nine = "IX",
-  ten = "X",
-  forty = "XL",
-  fifty = "L",
-  ninety = "XC",
-  oneHundred = "C",
-  fourHundred = "CD",
-  fiveHundred = "D",
-  nineHundred = "CM",
+interface NumeralValuePair {
+  value: number;
+  numeral: string;
 }
-export const parseToNumeral = (numeral: number): string => {
-  if (numeral > 999) {
+
+const numeralValues: NumeralValuePair[] = [
+  { value: 900, numeral: "CM" },
+  { value: 500, numeral: "D" },
+  { value: 400, numeral: "CD" },
+  { value: 100, numeral: "C" },
+  { value: 90, numeral: "XC" },
+  { value: 50, numeral: "L" },
+  { value: 40, numeral: "XL" },
+  { value: 10, numeral: "X" },
+  { value: 9, numeral: "IX" },
+  { value: 5, numeral: "V" },
+  { value: 4, numeral: "IV" },
+  { value: 1, numeral: "I" },
+];
+
+export const parseToNumeral = (number: number): string => {
+  if (number > 999) {
     return "Numeral too large!";
   }
 
-  if (numeral >= 900) {
-    return `${numeralsEnum.nineHundred}${parseToNumeral(numeral - 900)}`;
+  for (let i = 0; i < numeralValues.length; i++) {
+    const n = numeralValues[i];
+    if (number >= n.value) {
+      return `${n.numeral}${parseToNumeral(number - n.value)}`;
+    }
   }
 
-  if (numeral >= 500) {
-    return `${numeralsEnum.fiveHundred}${parseToNumeral(numeral - 500)}`;
-  }
-
-  if (numeral >= 400) {
-    return `${numeralsEnum.fourHundred}${parseToNumeral(numeral - 400)}`;
-  }
-  if (numeral >= 100) {
-    return `${numeralsEnum.oneHundred}${parseToNumeral(numeral - 100)}`;
-  }
-
-  if (numeral >= 90) {
-    return `${numeralsEnum.ninety}${parseToNumeral(numeral - 90)}`;
-  }
-
-  if (numeral >= 50) {
-    return `${numeralsEnum.fifty}${parseToNumeral(numeral - 50)}`;
-  }
-
-  if (numeral >= 40) {
-    return `${numeralsEnum.forty}${parseToNumeral(numeral - 40)}`;
-  }
-
-  if (numeral >= 10) {
-    return `${numeralsEnum.ten}${parseToNumeral(numeral - 10)}`;
-  }
-
-  if (numeral >= 9) {
-    return numeralsEnum.nine;
-  }
-
-  if (numeral >= 5) {
-    return `${numeralsEnum.five}${parseToNumeral(numeral - 5)}`;
-  }
-
-  if (numeral >= 4) {
-    return numeralsEnum.four;
-  }
-
-  if (numeral < 1) {
-    return "";
-  }
-
-  return `${numeralsEnum.one}${parseToNumeral(numeral - 1)}`;
+  return "";
 };
